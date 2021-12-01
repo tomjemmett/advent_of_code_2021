22622185ce3b76f95716914d9eefc26e52822a61
Tom Jemmett <thomas.jemmett@nhs.net>
2021-12-01 13:20:09 +0000

    refactors day 1

--- app/day01.hs
+++ app/day01.hs
@@ -5,29 +5,27 @@ module Day01 (
 import Data.List (tails)
 
 day01 :: String -> String
-day01 i = unlines [p1, p2]
+day01 input = unlines $ map (show . count_increments 0) [i, rollsum3 i]
   where
-    p1 = show $ day01_p1 0 input
-    p2 = show $ day01_p1 0 $ day01_p2 input
-    input = map read $ lines i
+    i = map read $ lines input
 
 -- for part 1, recursively run this function over the input to work out whether
 -- to incremement the counter (n) or not
-day01_p1 :: Int -> [Int] -> Int
+count_increments :: Int -> [Int] -> Int
 -- base case: we have only one item left in the list, so just return n
-day01_p1 n [_] = n
+count_increments n [_] = n
 -- recursive case: take the first two items from the list (a and b), and compare
 -- them. if b is greater than a then add one to n'.
-day01_p1 n (a:b:xs)
+count_increments n (a:b:xs)
   | b > a = 1 + n'
   | otherwise = n'
   where
-    n' = day01_p1 n (b:xs) -- recursive call, drop a and run on b and the rest
+    n' = count_increments n (b:xs) -- recursive call, drop a and run on b and the rest
 
 -- for part 2 we need to implement a function that will first give us the rolling
 -- window sum of width 3 of the input, then we can call the part 1 function
-day01_p2 :: [Int] -> [Int]
-day01_p2 = map sum
+rollsum3 :: [Int] -> [Int]
+rollsum3 = map sum
   . filter((3 ==) . length)
   . map (take 3)
   . tails
