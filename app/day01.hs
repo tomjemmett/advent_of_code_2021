07e527100529e0ca887a340a3ad28d48edb5c631
Tom Jemmett <tom.jemmett@gmail.com>
2021-12-02 21:02:31 +0000

    refactors day 1

--- app/day01.hs
+++ app/day01.hs
@@ -6,7 +6,7 @@ import Common
 import Data.List (tails)
 
 day01 :: String -> String
-day01 input = unlines $ map show [p1, p2]
+day01 input = unlines [p1, p2]
   where
     i = map read $ lines input
     p1 = countIncrements 1 i
@@ -15,5 +15,5 @@ day01 input = unlines $ map show [p1, p2]
 -- check that each item in the list is less than the nth next item
 -- for part 1, n = 1 (the next item)
 -- for part 2, n = 3: sum (a, b, c) < sum (b, c, d) iff a < d
-countIncrements :: Int -> [Int] -> Int
-countIncrements n x = countTrue id $ zipWith (<) x (drop n x)
+countIncrements :: Int -> [Int] -> String
+countIncrements n x = show $ countTrue id $ zipWith (<) x (drop n x)
