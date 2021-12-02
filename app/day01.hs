50f77ecf14fccfe939f670a5b166830bcb50d73e
Tom Jemmett <tom.jemmett@gmail.com>
2021-12-02 20:53:16 +0000

    refactors day 1

--- app/day01.hs
+++ app/day01.hs
@@ -2,30 +2,18 @@ module Day01 (
   day01  
 ) where
 
+import Common
 import Data.List (tails)
 
 day01 :: String -> String
-day01 input = unlines $ map (show . countIncrements 0) [i, rollSum3 i]
+day01 input = unlines $ map show [p1, p2]
   where
     i = map read $ lines input
+    p1 = countIncrements 1 i
+    p2 = countIncrements 3 i
 
--- for part 1, recursively run this function over the input to work out whether
--- to incremement the counter (n) or not
+-- check that each item in the list is less than the nth next item
+-- for part 1, n = 1 (the next item)
+-- for part 2, n = 3: sum (a, b, c) < sum (b, c, d) iff a < d
 countIncrements :: Int -> [Int] -> Int
--- base case: we have only one item left in the list, so just return n
-countIncrements n [_] = n
--- recursive case: take the first two items from the list (a and b), and compare
--- them. if b is greater than a then add one to n'.
-countIncrements n (a:b:xs)
-  | b > a = 1 + n'
-  | otherwise = n'
-  where
-    n' = countIncrements n (b:xs) -- recursive call, drop a and run on b and the rest
-
--- for part 2 we need to implement a function that will first give us the rolling
--- window sum of width 3 of the input, then we can call the part 1 function
-rollSum3 :: [Int] -> [Int]
-rollSum3 = map sum
-  . filter((3 ==) . length)
-  . map (take 3)
-  . tails
+countIncrements n x = countTrue id $ zipWith (<) x (drop n x)
