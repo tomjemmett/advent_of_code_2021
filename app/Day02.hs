3cc5e90089c1cec3e47065e25618038ad771fad5
Tom Jemmett <tom.jemmett@gmail.com>
2021-12-02 21:31:32 +0000

    refactors day 2

--- app/Day02.hs
+++ app/Day02.hs
@@ -11,25 +11,25 @@ type PositionUpdate = (Int, Int, Int)
 day02 :: String -> String
 day02 input = unlines $ map answer [p1, p2]
   where
-      answer p = show $ forward p * depth p
-      p1 = foldl part1 initPosition i
-      p2 = foldl part2 initPosition i
-      initPosition = Position 0 0 0
-      i = linesWords input
+    answer p = show $ forward p * depth p
+    p1 = foldl part1 initPosition i
+    p2 = foldl part2 initPosition i
+    initPosition = Position 0 0 0
+    i = linesWords input
 
 part1 :: Position -> [String] -> Position
 part1 p [x, y] = updatePosition p $ case x of
-  "forward" -> (y', 0,  0)
-  "down"    -> (0,  y', 0)
-  "up"      -> (0, -y', 0)
-  where y' = read y
+  "forward" -> (z,  0, 0)
+  "down"    -> (0,  z, 0)
+  "up"      -> (0, -z, 0)
+  where z = read y
 
 part2 :: Position -> [String] -> Position
-part2 p [x, y] = case x of
-  "forward" -> updatePosition p (y', y' * aim p,   0)
-  "down"    -> updatePosition p ( 0,          0,  y')
-  "up"      -> updatePosition p ( 0,          0, -y')
-  where y' = read y
+part2 p [x, y] = updatePosition p $ case x of
+  "forward" -> (z, z * aim p, 0)
+  "down"    -> (0, 0,  z)
+  "up"      -> (0, 0, -z)
+  where z = read y
 
 updatePosition :: Position -> PositionUpdate -> Position
 updatePosition p (x, y, z) = Position f d a
