71954a879338cda8ab912e2ee310f1b5ca64480d
Tom Jemmett <tom.jemmett@gmail.com>
2021-12-02 20:27:19 +0000

    refactor day 2

--- app/day02.hs
+++ app/day02.hs
@@ -4,32 +4,32 @@ module Day02 (
 
 data Position = Position { forward :: Int, depth :: Int, aim :: Int } deriving (Show)
 
+type PositionUpdate = (Int, Int, Int)
+
 day02 :: String -> String
 day02 input = unlines $ map answer [p1, p2]
   where
       answer p = show $ forward p * depth p
-      p1 = foldl parseLineP1 initPosition i
-      p2 = foldl parseLineP2 initPosition i
+      p1 = foldl part1 initPosition i
+      p2 = foldl part2 initPosition i
       initPosition = Position 0 0 0
       i = map words $ lines input
 
-parseLineP1 :: Position -> [String] -> Position
-parseLineP1 p [x, y]
-  | x == "forward" = updatePosition p (y',  0, 0)
-  | x == "down"    = updatePosition p (0,  y', 0)
-  | x == "up"      = updatePosition p (0, -y', 0)
-  where
-    y' = read y
+part1 :: Position -> [String] -> Position
+part1 p [x, y] = updatePosition p $ case x of
+  "forward" -> (y', 0,  0)
+  "down"    -> (0,  y', 0)
+  "up"      -> (0, -y', 0)
+  where y' = read y
 
-parseLineP2 :: Position -> [String] -> Position
-parseLineP2 p [x, y]
-  | x == "forward" = updatePosition p (y', y' * aim p,   0)
-  | x == "down"    = updatePosition p ( 0,          0,  y')
-  | x == "up"      = updatePosition p ( 0,          0, -y')
-  where
-    y' = read y
+part2 :: Position -> [String] -> Position
+part2 p [x, y] = case x of
+  "forward" -> updatePosition p (y', y' * aim p,   0)
+  "down"    -> updatePosition p ( 0,          0,  y')
+  "up"      -> updatePosition p ( 0,          0, -y')
+  where y' = read y
 
-updatePosition :: Position -> (Int, Int, Int) -> Position
+updatePosition :: Position -> PositionUpdate -> Position
 updatePosition p (x, y, z) = Position f d a
   where
     f = forward p + x
