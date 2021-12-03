987963cee0836d597fcdfb0a0cad9baad906b0e5
Tom Jemmett <thomas.jemmett@nhs.net>
2021-12-03 13:21:32 +0000

    refactor day 3

--- app/Day03.hs
+++ app/Day03.hs
@@ -5,6 +5,10 @@ module Day03 (
 import Common
 import Data.List (sort, group)
 
+type Input = [String]
+type StepFilter = Char -> [String] -> [String]
+type Comparer = (Int -> Int -> Bool)
+
 testInput = ["00100",
              "11110",
              "10110",
@@ -19,34 +23,28 @@ testInput = ["00100",
              "01010"]
 
 day03 :: String -> String
-day03 input = unlines [p1, p2]
+day03 input = unlines $ [p1, p2] <*> pure i
   where
     i = lines input
-    p1 = part1 i
-    p2 = part2 i
+    p1 = runPart (const id)                    -- we don't want to filter in part 1, so we can use the constant identity
+    p2 = runPart (\b -> filter ((== b). head)) -- filter where the first item in the input matches the bigger item
 
-part1 :: [String] -> String
-part1 input = show $ gamma input * epsillon input
-  where
-    gamma = bitStringToInt . p1 (>) ""
-    epsillon = bitStringToInt . p1 (<=) ""
-    p1 = d3 (const id) -- we don't want to filter in part 1, so we can use the constant identity
+runPart :: StepFilter -> Input -> String
+runPart stepFilter input = show $ -- turn Int into a string
+  product $                       -- multiply the values
+  map bitStringToInt $            -- convert the bit strings to integers
+  -- create two versions of our day 3 function for > and <= as the comparer argument
+  -- we can use and Applicative to run each function on the LHS with the input (which needs to be wrapped in pure)
+  map (d3 stepFilter "") [(>), (<=)] <*> pure input
 
-part2 :: [String] -> String
-part2 input = show $ oxygen input * co2Scruber input
-  where
-    oxygen = bitStringToInt . p2 (>) ""
-    co2Scruber = bitStringToInt . p2 (<=) ""
-    p2 = d3 (\b -> filter ((== b) . head)) -- filter where the first item in the input matches the bigger item
-
-d3 :: (Char -> [String] -> [String]) -> -- a function which will filter the input list
-  (Int -> Int -> Bool) ->               -- a comparison function, should be either (>) or (<=)
-  String ->                             -- a string containing the current match (first iteration should be "")
-  [String] ->                           -- the input string, where each item is a line from the file
-  String                                -- return a string of the result
-d3 _ _ c ("":xs) = c  -- exhausted length of input strings, return the current match
-d3 _ _ c [i] = c ++ i -- single element, return the current match with the single item
-d3 stepFilter comparer c input = d3 stepFilter comparer c' nextInput -- recursively call
+d3 :: StepFilter -> -- a function which will filter the input list
+  String         -> -- a string containing the current match (first iteration should be "")
+  Comparer       -> -- a comparison function, should be either (>) or (<=)
+  Input          -> -- the input string, where each item is a line from the file
+  String            -- return a string of the result
+d3 _ c _ ("":xs) = c  -- exhausted length of input strings, return the current match
+d3 _ c _ [i] = c ++ i -- single element, return the current match with the single item
+d3 stepFilter c comparer input = d3 stepFilter c' comparer nextInput -- recursively call
   where
     -- consider just the first character
     firstChars = map head input
