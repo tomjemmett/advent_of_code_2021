1da4484fc8ddc6a156c014eafcdb032bb66cc2f2
Tom Jemmett <tom.jemmett@gmail.com>
2021-12-04 21:06:10 +0000

    moves days into a library

--- CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-# Revision history for aoc2021
-
-## 0.0.1 -- 2021-12-01
-
-Added day 1 solution
-
-## 0.0.0 -- 2021-12-01
-
-Created repository
--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,7 @@
 module Main where
 
 import System.IO ( hGetContents, withFile, IOMode(ReadMode) )
-import Day01
-import Day02
-import Day03
-import Day04
-import Day05
-import Day06
-import Day07
-import Day08
-import Day09
-import Day10
-import Day11
-import Day12
-import Day13
-import Day14
-import Day15
-import Day16
-import Day17
-import Day18
-import Day19
-import Day20
-import Day21
-import Day22
-import Day23
-import Day24
-import Day25
+import Days
 
 main :: IO ()
 main = do
