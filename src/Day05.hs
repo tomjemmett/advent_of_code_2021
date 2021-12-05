e55f669aa0a64a4e6aea85cf4c27a466b2eeb7a0
Tom Jemmett <tom.jemmett@gmail.com>
2021-12-05 11:41:08 +0000

    refactors  day 5

--- src/Day05.hs
+++ src/Day05.hs
@@ -8,21 +8,19 @@ import qualified Data.Map as M
 import Data.Map ((!))
 import Data.List.Split (splitOn)
 
-type Point = (Int, Int)
-type Line  = (Point, Point)
-type Lines = [Line]
+type Point  = (Int, Int)
+type Points = [Point]
+type Line   = (Point, Point)
+type Lines  = [Line]
+type Grid   = M.Map Point Int
 
 day05 :: AOCSolution
 day05 input = [p1 i, p2 i]
   where
     i = parseInput input
 
-makeGrid :: M.Map (Int, Int) Int
-makeGrid = M.fromList $ zip keys $ repeat 0
-  where
-    sz = [0..999]
-    lg = length sz
-    keys = concat $ map (zip sz . replicate lg) sz
+makeGrid :: Grid
+makeGrid = M.empty
 
 parseInput :: String -> Lines
 parseInput = map parseLine . filter (/= "") . lines
@@ -31,21 +29,22 @@ parseInput = map parseLine . filter (/= "") . lines
       where
         [[a, b], [c, d]] = map2 read . map (splitOn ",") $ splitOn " -> " l
 
-p1 lines = show $ length $ filter((>= 2)) $ M.elems grid
-  where
-    lines' = filter (\((a, b), (c, d)) -> a == c || b == d) lines
-    grid = foldl processLine makeGrid lines'
+p1 :: Lines -> String
+p1 = p2 . filter (\((a, b), (c, d)) -> a == c || b == d)
 
+p2 :: Lines -> String
 p2 lines = show $ length $ filter((>= 2)) $ M.elems grid
   where
     grid = foldl processLine makeGrid lines
 
+processLine :: Grid -> Line -> Grid
 processLine grid line = grid'
   where
     grid' = foldl processPoint grid points
     points = lineToPoints line
-    processPoint = flip (M.adjust (+1))
+    processPoint = flip $ flip (M.insertWith (+)) 1
 
+lineToPoints :: Line -> Points
 lineToPoints (ab@(a, b), cd@(c, d))
   | a == c && b <  d = ab : lineToPoints ((a, b + 1), cd)
   | a == c && b >  d = ab : lineToPoints ((a, b - 1), cd)
