f8d9f89f9acf7b2a21a344a4337693a639b39199
Tom Jemmett <tom.jemmett@gmail.com>
2021-12-05 13:05:49 +0000

    refactors day 5, uses parsec

--- src/Day05.hs
+++ src/Day05.hs
@@ -3,9 +3,11 @@ module Day05 (
 ) where
 
 import Common
+import Data.Either (fromRight)
+import Data.List.Split (splitOn)
 import qualified Data.Map as M
 import Data.Map ((!))
-import Data.List.Split (splitOn)
+import qualified Text.ParserCombinators.Parsec as P
 
 type Point  = (Int, Int)
 type Points = [Point]
@@ -14,35 +16,49 @@ type Lines  = [Line]
 type Grid   = M.Map Point Int
 
 day05 :: AOCSolution
-day05 input = show <$> d5 <$> ([p1, p2] <*> pure (parseInput input))
+day05 input = fmap (show . d5) $ [f, id] <*> parseInput input
   where
-    p1 = filter (\((a, b), (c, d)) -> a == c || b == d)
-    p2 = id
-
-parseInput :: String -> Lines
-parseInput = map parseLine . filter (/= "") . lines
-  where
-    parseLine l = ((a, b), (c, d))
-      where
-        [[a, b], [c, d]] = map2 read . map (splitOn ",") $ splitOn " -> " l
+    -- for part 1, remove diagnoal lines
+    f = filter (\((a, b), (c, d)) -> a == c || b == d)
 
 d5 :: Lines -> Int
-d5 = length .
-  filter((>= 2)) .
-  M.elems .
-  foldl processLine M.empty
+d5 = length .                -- get the length of remaining items in the list, our result
+  filter((>= 2)) .           -- remove points from our "grid" which were matched less than twice
+  M.elems .                  -- get the elements from the map
+  foldl processLine M.empty  -- use an empty map to store our grid, then fold over the input (lines) to update the grid
 
 processLine :: Grid -> Line -> Grid
 processLine grid line = grid'
   where
-    grid' = foldl processPoint grid points
-    points = lineToPoints line
-    processPoint = flip $ flip (M.insertWith (+)) 1
+    grid' = foldl processPoint grid points          -- process all of the points on this line
+    points = lineToPoints line                      -- extract the points on this line
+    processPoint = flip $ flip (M.insertWith (+)) 1 -- update the grid by incrementing the value at that position
 
+-- convert a Line to the set of Points on that line
 lineToPoints :: Line -> Points
 lineToPoints (ab@(a, b), cd@(c, d))
-  | a == c && b == d = [ab]
-  | a == c           = ab : lineToPoints ((a, f b d), cd)
-  | b == d           = ab : lineToPoints ((f a c, b), cd)
-  | otherwise        = ab : lineToPoints ((f a c, f b d), cd)
+  | ab == cd  = [ab] -- ab and cd are the same point, so just return it
+  -- the following cases will handle where the cases where:
+  -- * first direction isn't changed
+  -- * second direction isn't changed
+  -- * both directions change (diagonal)
+  | a == c    = ab : lineToPoints ((a, f b d), cd)
+  | b == d    = ab : lineToPoints ((f a c, b), cd)
+  | otherwise = ab : lineToPoints ((f a c, f b d), cd)
+  -- this function will either increment or decrement the first value to make it approach the second value
   where f x y = (if x < y then succ else pred) x
+
+parseInput :: Applicative f => String -> f Lines
+parseInput = pure . map (fromRight ((0,0), (0,0)) . P.parse p "") . lines
+  where
+    f [a, b, c, d] = ((a, b), (c, d))
+    p :: P.CharParser () Line
+    p = do
+      a <- P.many1 P.digit -- read 1 or more digits
+      P.char ','           -- comma is ignored
+      b <- P.many1 P.digit -- read 1 or more digits
+      P.string " -> "      -- arrow is ignored
+      c <- P.many1 P.digit -- read 1 or more digits
+      P.char ','           -- comma is ignored
+      d <- P.many1 P.digit -- read 1 or more digits
+      return $ f $ map read [a, b, c, d]
