1880e0c73bf72a95fd510e371a5677b515132298
Tom Jemmett <tom.jemmett@gmail.com>
2021-12-06 07:10:52 +0000

    refactor day 6

--- src/Day06.hs
+++ src/Day06.hs
@@ -4,17 +4,23 @@ module Day06 (
 
 import Common
 import Data.List (group, sort)
-import qualified Data.Map as M
 
 day06 :: AOCSolution
-day06 input = show . d6 x <$> [80, 256]
+day06 input = show . d6 i <$> [80, 256]
   where
-    i = group $ sort $ commaSeparatedInts input
-    m = foldr (`M.insert` 0 ) M.empty [0..8]
-    x = M.elems $ foldr (\k -> M.insertWith (+) (head k) (length k)) m i
+    -- take the input, and append the full list of days (0 to 8)
+    -- then, sort this list, group the items, and calculate the length
+    -- this will tell us how many of each day appears in our input, but
+    -- because we added all days we need to subtract one from the length
+    --
+    -- that is, we have a list counting how many fish are alive on each day
+    i = map (pred . length) $ group $ sort $ commaSeparatedInts input ++ [0..8]
 
 d6 :: [Int] -> Int -> Int
-d6 xs 0 = sum xs
+d6 xs 0 = sum xs -- reached last day, return the sum of our count list
 d6 (x:xs) d = d6 xs' $ pred d
   where
+    -- drop the first item from the input, shifting the days along to the left
+    -- then, add to the 6th day the value from day 0, and append day 0 to the
+    -- end of the list
     xs' = zipWith (+) [0,0,0,0,0,0,x,0] xs ++ [x]
