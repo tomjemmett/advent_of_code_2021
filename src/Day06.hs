1782721b6134b2aa2a400a83ce5fd5075d37028d
Tom Jemmett <thomas.jemmett@nhs.net>
2021-12-06 10:39:36 +0000

    refactors day 6 to use iterate

--- src/Day06.hs
+++ src/Day06.hs
@@ -5,22 +5,18 @@ module Day06 (
 import Common
 import Data.List (group, sort)
 
+-- we can solve today's problem by representing the input as a list of integers, representing the number of fish that
+-- are alive on that day.
+-- we take the input and seperate into a list of integers, then append the numbers 0 through 8 (this ensures each day
+-- is listed, but does mean that we have 1 extra fish per day). if we sort the list, we can then group the numbers up
+-- like: [0,1,1,2,2,2] => [[0], [1,1], [2,2,2]]
+-- then, we can work out the length of each item, subtracting 1 from the value. The above example would become [0,1,2].
 day06 :: AOCSolution
-day06 input = show . d6 i <$> [80, 256]
+day06 input = show . sum . (!!) (iterate d6 i) <$> [80, 256]
   where
-    -- take the input, and append the full list of days (0 to 8)
-    -- then, sort this list, group the items, and calculate the length
-    -- this will tell us how many of each day appears in our input, but
-    -- because we added all days we need to subtract one from the length
-    --
-    -- that is, we have a list counting how many fish are alive on each day
     i = map (pred . length) $ group $ sort $ commaSeparatedInts input ++ [0..8]
 
-d6 :: [Int] -> Int -> Int
-d6 xs 0 = sum xs -- reached last day, return the sum of our count list
-d6 (x:xs) d = d6 xs' $ pred d
-  where
-    -- drop the first item from the input, shifting the days along to the left
-    -- then, add to the 6th day the value from day 0, and append day 0 to the
-    -- end of the list
-    xs' = zipWith (+) [0,0,0,0,0,0,x,0] xs ++ [x]
+-- drop the first item from the input, shifting the days along to the left
+-- then, add to the 6th day the value from day 0, and append day 0 to the end of the list
+d6 :: [Int] -> [Int]
+d6 (x:xs) = zipWith (+) [0,0,0,0,0,0,x,0] xs ++ [x]
