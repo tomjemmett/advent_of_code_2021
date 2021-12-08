2f5fd624548d63873a728c19c46ba11e14876e3e
Tom Jemmett <thomas.jemmett@nhs.net>
2021-12-08 12:41:11 +0000

    refactors day 8

--- src/Day08.hs
+++ src/Day08.hs
@@ -1,93 +1,50 @@
-module Day08 (
-  day08, parseInput
+module Day08 where
+{-  
+(
+  day08
 ) where
+-}
 
 import Common
-import Data.List (sort, intersect, union, (\\))
+import Data.List (sort, sortBy, groupBy, intersect, union, (\\))
 import Data.List.Split (splitOn)
+import Data.Function (on)
 import qualified Data.Map as M
 import Data.Map ((!))
 
 day08 :: AOCSolution
-day08 input = [show $ foldl part1 0 i, show $ sum $ map part2 i]
+day08 input = show <$> (foldl <$> [part1, part2] <*> pure 0 <*> pure i)
   where
-    i = parseInput input
+    i = map (splitOn ["|"] . map sort . words) $ lines input
 
 part1 :: Int -> [[String]] -> Int
-part1 n [_, b] = n + b'
-  where
-    b' = countTrue (`elem` [2, 4, 3, 7]) $ map length b
+part1 n [_, b] = n + (countTrue (`elem` [2, 4, 3, 7]) $ map length b) 
 
-part2 [a, b] = sum $ zipWith (*) [1000, 100, 10, 1] $ map (m !) $ map sort b
+part2 :: Int -> [[String]] -> Int
+part2 n [a, b] = n + foldl (\x y -> x * 10 + (m ! y)) 0 b
   where
-    fl n = filter (\x -> length x == n)
-    a1 = head $ fl 2 a
-    a4 = head $ fl 4 a
-    a7 = head $ fl 3 a
-    a8 = head $ fl 7 a
-    l5 = fl 5 a
-    l6 = fl 6 a
+    -- head filter: get the first thing filter finds
+    hfilter f = head . (filter f)
+    -- group the inputs on length
+    [l2, l3, l4, l5, l6, l7] = groupBy ((==) `on` length) $ sortBy (compare `on` length) a
+    -- these are the initial values that we know because they are unique
+    [a1, a7, a4, a8] = head <$> [l2, l3, l4, l7]
     -- first, consider what's same between a1 and a7, that should tell us what is 'a'
-    pa = head $ a7 \\ a1
-    -- c/f can be found from the intersection of a1 and a7
+    pa = a7 \\ a1
+    -- 'c' and 'f' can be found from the intersection of a1 and a7
     pcf = a1 `intersect` a7
-    -- b/d can be found from difference between a4 and a1, but can't tell their positions
-    pbd = a4 \\ a1
-    -- e/g can be found from difference between a8 and a7 and a4
-    peg = a8 \\ (a4 `union` a7)
-    -- we can find 3 now: it's going to be the only 5 length with f set
-    a3 = head $ filter (\x -> length (x \\ pcf) == 3) l5
-    -- which leads to g and d
-    pg = head $ a3 \\ (a4 `union` a7)
-    pd = head $ a3 \\ (a7 `union` [pg])
-    -- we can now solve d and find 2 and 5
-    pe = head $ head $ fl 1 $ map (\\ (a4 `union` [pa, pd, pg])) (l5 \\ [a3])
-    a2 = head $ filter (pe `elem`) l5
+    -- we can find 3 now: it's going to be the only 5 length with an 'f'
+    a3 = hfilter (\x -> length (x \\ pcf) == 3) l5
+    -- which leads to 'g' and 'd'
+    pg = a3 \\ (a4 `union` a7)
+    pd = a3 \\ (a7 `union` pg)
+    -- we can now solve 'e' and find 2 and 5
+    pe = hfilter (not . null) $ map (\\ (foldr1 union [a4, pa, pd, pg])) (l5 \\ [a3])
+    a2 = hfilter (not . null . intersect pe) l5
     a5 = head $ l5 \\ [a2, a3]
-    -- with 5 we can find b and c
-    pb = head $ a5 \\ (a1 `union` [pa, pd, pg])
-    pc = head $ a1 \\ a5
-    -- f is now findable
-    pf = head $ a8 \\ [pa, pb, pc, pd, pe, pg]
     -- now we can reconstruct the rest
-    a0 = head $ filter (pd `notElem`) l6
-    a6 = head $ filter (pe `elem`) (l6 \\ [a0])
+    a0 = hfilter (null . intersect pd) l6
+    a6 = hfilter (not . null . intersect pe) (l6 \\ [a0])
     a9 = head $ l6 \\ [a0, a6]
-
-    m = M.fromList [
-      (sort a0, 0),
-      (sort a1, 1),
-      (sort a2, 2),
-      (sort a3, 3),
-      (sort a4, 4),
-      (sort a5, 5),
-      (sort a6, 6),
-      (sort a7, 7),
-      (sort a8, 8),
-      (sort a9, 9)]
-
-
-
-parseInput :: String -> [[[String]]]
-parseInput = map (map words . splitOn "|") . lines
-
-segmentN s = case sort s of
-  "abcefg"  -> 0
-  "cf"      -> 1
-  "acdeg"   -> 2
-  "acdfg"   -> 3
-  "bcdf"    -> 4
-  "abdfg"   -> 5
-  "abdefg"  -> 6
-  "acf"     -> 7
-  "abcdefg" -> 8
-  "abcdfg"  -> 9
-
-t = \case
-  'a' -> 'a'
-  'c' -> 'b'
-  'e' -> 'c'
-  'd' -> 'd'
-  'g' -> 'e'
-  'f' -> 'f'
-  'b' -> 'g'
+    -- create a map so we can easily lookup using b
+    m = M.fromList $ zip [a0, a1, a2, a3, a4, a5, a6, a7, a8, a9] [0..]
