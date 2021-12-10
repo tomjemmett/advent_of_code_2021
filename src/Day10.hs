059664f243e2485790da3ea7481958d4860771c1
Tom Jemmett <thomas.jemmett@nhs.net>
2021-12-10 13:23:58 +0000

    refactors day 10

--- src/Day10.hs
+++ src/Day10.hs
@@ -8,25 +8,23 @@ import Data.List (sort)
 data NavigationSystem = Complete | Incomplete | Corrupted deriving (Show, Eq)
 
 day10 :: AOCSolution
-day10 input = show <$> ([p1, p2] <*> pure i)
+day10 input = map show $ f <$> [(sum, Corrupted), (head . median, Incomplete)] <*> pure i
   where
     i = map (parseLine "") $ lines input
-    f s x = s . map snd . filter ((== x) . fst)
-    p1 = f sum Corrupted
-    p2 = f (head . median) Incomplete
+    f (s, x) = s . map snd . filter ((== x) . fst)
 
 parseLine :: String -> String -> (NavigationSystem, Int)
 parseLine [] [] = (Complete, 0)
 parseLine st [] = (Incomplete, incompleteScore st)
 parseLine st (x:xs)
-  | x `elem` "([<{"      = parseLine (x:st) xs
-  | x == ')' && s == '(' = parseLine ss xs
-  | x == ']' && s == '[' = parseLine ss xs
-  | x == '>' && s == '<' = parseLine ss xs
-  | x == '}' && s == '{' = parseLine ss xs
-  | otherwise            = (Corrupted, corruptedScore x)
+  | x `elem` "([<{"   = parseLine (x:st) xs
+  | validPair (s:[x]) = parseLine ss xs
+  | otherwise         = (Corrupted, corruptedScore x)
   where
     (s:ss) = st
+
+validPair :: String -> Bool
+validPair = flip elem ["()", "[]", "<>", "{}"]
 
 corruptedScore :: Char -> Int
 corruptedScore = \case
