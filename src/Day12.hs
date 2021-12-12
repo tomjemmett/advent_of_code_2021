2ecff03e58f962b3e9a51d970410d3ebe4e9fa26
Tom Jemmett <thomas.jemmett@nhs.net>
2021-12-12 21:05:34 +0000

    improves performance
    * uses HashSets to record what has been visited
    * avoids costly list concatenation by using foldl with flip (:)

--- src/Day12.hs
+++ src/Day12.hs
@@ -8,6 +8,7 @@ import Data.Function (on)
 import Data.Graph
 import Data.List (sortBy, groupBy, intercalate)
 import Data.List.Split (splitOn)
+import qualified Data.HashSet as HS
 
 type D12Graph = (Graph, Vertex -> ((), String, [String]), String -> Maybe Vertex)
 
@@ -15,8 +16,8 @@ day12 :: AOCSolution
 day12 input = [show p1, show p2]
   where
     i = parseInput input
-    p1 = length $ mbfs [] [("start", ["start"], True)] i
-    p2 = length $ mbfs [] [("start", ["start"], False)] i
+    p1 = mbfs 0 [("start", HS.singleton "start", True)] i
+    p2 = mbfs 0 [("start", HS.singleton "start", False)] i
 
 parseInput :: String -> D12Graph
 parseInput = graphFromEdges .
@@ -28,15 +29,14 @@ parseInput = graphFromEdges .
   where
     f [a, b] = filter (\(x, y) -> x /= "end" && y /= "start") [(a, b), (b,a)]
 
-mbfs :: [[String]] -> [(String, [String], Bool)] -> D12Graph -> [String]
-mbfs p _ _ | length p == 50000 = map (intercalate "-" . reverse) p
-mbfs p [] _ = map (intercalate "-" . reverse) p
-mbfs p (("end", y, _):xs) (g, v, n) = mbfs (y:p) xs (g, v, n)
+mbfs :: Int -> [(String, HS.HashSet String, Bool)] -> D12Graph -> Int
+mbfs p [] _ = p
+mbfs p (("end", y, _):xs) (g, v, n) = mbfs (succ p) xs (g, v, n)
 mbfs p ((x, y, z):xs) (g, v, n) = mbfs p xs' (g, v, n)
   where
-    previouslyVisited = flip elem y
+    previouslyVisited = flip HS.member y
     isBigCave = not . all isLower
     (_, _, e) = maybe ((), x, []) v (n x)
     e' = filter (\x -> isBigCave x || not (previouslyVisited x) || not z) e
-    z' = map (\ee -> z || (not (isBigCave ee) && previouslyVisited ee)) e  
-    xs' = xs ++ zip3 e' (map (:y) e') z'
+    z' = map (\x -> z || (not (isBigCave x) && previouslyVisited x)) e
+    xs' = foldl (flip (:)) xs (zip3 e' (map (`HS.insert` y) e') z')
