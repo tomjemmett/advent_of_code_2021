ba8813061e3b7d4d5f6398385908bfe4ae19441d
Tom Jemmett <thomas.jemmett@nhs.net>
2021-12-16 09:00:46 +0000

    adds part 1 day 15, but runs very slowly

--- src/Day15.hs
+++ src/Day15.hs
@@ -3,6 +3,38 @@ module Day15 (
 ) where
 
 import Common
+import Control.Monad (liftM2)
+import Data.Char (digitToInt)
+import qualified Data.Map as M
+import Data.Map ((!))
+import qualified Data.PQueue.Prio.Min as PQ
+import qualified Data.HashSet as S
+
+type Graph = M.Map Point2d Int
 
 day15 :: AOCSolution
-day15 = const ["", ""]
+day15 input = [show p1, ""]
+  where
+    i = parseInput input
+    size = length (lines input) - 1
+    p1 = bfs i (size, size) S.empty (PQ.singleton 0 (0, 0))
+
+parseInput :: String -> Graph
+parseInput input = M.fromList (zip p (concat i))
+  where
+    i = map2 digitToInt $ lines input
+    p = concat $ (zipWith3 . zipWith3) (const (,)) i (repeat [0..]) (map repeat [0..])
+
+bfs :: Graph -> Point2d -> S.HashSet Point2d -> PQ.MinPQueue Int Point2d -> Int
+bfs g t v pq
+  | s == t    = d + (g ! t) - (g ! (0, 0))
+  | otherwise = bfs g t v' pq''
+  where
+    ((d, s), pq') = PQ.deleteFindMin pq
+    d' = (g ! s) + d
+    n = filter ((not . flip S.member v) <&&> flip M.member g) $ point2dNeighbours s
+    pq'' = foldr (uncurry PQ.insert . (d',)) pq' n
+    v' = S.insert s v
+
+(<&&>) :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+(<&&>) = liftM2 (&&)
--- test/Day15Spec.hs
+++ test/Day15Spec.hs
@@ -2,14 +2,25 @@ module Day15Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
-
+testInput = "1163751742\n\
+            \1381373672\n\
+            \2136511328\n\
+            \3694931569\n\
+            \7463417111\n\
+            \1319128137\n\
+            \1359912421\n\
+            \3125421639\n\
+            \1293138521\n\
+            \2311944581"
+            
 spec :: Spec
 spec = describe "Day 15" $ do
   it "Sample" $ do
-    day15 testInput `shouldBe` ["", ""]
+    day15 testInput `shouldBe` ["40", ""]
 
+  {- ignoring day 15 as it runs too slowly!
   it "Actual" $ do
     withFile "inputs/day15.txt" ReadMode (\h -> do
       actualInput <- hGetContents h
-      day15 actualInput `shouldBe` ["",""])
+      day15 actualInput `shouldBe` ["687",""])
+  -}
