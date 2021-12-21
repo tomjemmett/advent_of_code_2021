8601d2f9bdca0092f026f0854508453f27abb7b8
Tom Jemmett <thomas.jemmett@nhs.net>
2021-12-21 08:43:57 +0000

    adds day21 solution

--- src/Day21.hs
+++ src/Day21.hs
@@ -1,8 +1,60 @@
 module Day21 (
-  day21
+  day21, parseInput, part1
 ) where
 
 import Common
+import Data.Char (digitToInt)
+import Control.Monad.State (MonadState(get), modify, evalState, replicateM, State)
+import qualified Data.Map as M
+import Data.Tuple (swap)
+
+type Position = Int
+type Score = Int 
+type Player = (Position, Score)
+type Game = (Player, Player)
 
 day21 :: AOCSolution
-day21 = const ["", ""]
+day21 input = show <$> ([p1, p2] <*> i)
+  where
+    i = pure (parseInput input)
+    p1 = part1 0
+    p2 = part2
+
+parseInput :: String -> Game
+parseInput = tuplify2 . map ((,0) . digitToInt . last) . lines
+
+movePlayer :: Position -> Int -> Position
+movePlayer p n = (p - 1 + n) `mod` 10 + 1
+
+part1 :: Int -> Game -> Int
+part1 n ((p1, s1), (p2, s2))
+  | s1' >= 1000 = s2  * (n + 3)
+  | s2' >= 1000 = s1' * (n + 6)
+  | otherwise = part1 (n + 6) ((p1', s1'), (p2', s2'))
+  where
+    ns = map (succ . (`mod` 100) . (+n)) [0..5]
+    p1' = movePlayer p1 $ sum (take 3 ns)
+    p2' = movePlayer p2 $ sum (drop 3 ns)
+    s1' = s1 + p1'
+    s2' = s2 + p2'
+
+part2 :: Game -> Int
+part2 startGame = max x y
+  where
+    (x, y) = evalState (part2' startGame) M.empty
+    possibleRoles = sum <$> replicateM 3 [1, 2, 3]
+    part2' :: Game -> State (M.Map Game (Int, Int)) (Int, Int)
+    part2' game@((p1, s1), (p2, s2))
+      | s1 >= 21 = return (1, 0)
+      | s2 >= 21 = return (0, 1)
+      | otherwise = do
+        memoisedGames <- get
+        case M.lookup game memoisedGames of
+          Just r -> return r
+          _      -> do
+            let allOutcomes = [((p2, s2), (p1', p1' + s1)) | d <- possibleRoles, let p1' = movePlayer p1 d]
+            r <- swap .
+              foldl1 (\(a, b) (c, d) -> (a + c, b + d)) <$>
+              mapM part2' allOutcomes
+            modify (M.insert game r)
+            return r
--- test/Day21Spec.hs
+++ test/Day21Spec.hs
@@ -2,16 +2,15 @@ module Day21Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "Player 1 starting position: 4\n\
+            \Player 2 starting position: 8"
 
 spec :: Spec
 spec = describe "Day 21" $ do
   it "Sample" $ do
-    day21 testInput `shouldBe` ["", ""]
+    day21 testInput `shouldBe` ["739785", "444356092776315"]
 
-{-
   it "Actual" $ do
     withFile "inputs/day21.txt" ReadMode (\h -> do
       actualInput <- hGetContents h
-      day21 actualInput `shouldBe` ["",""])
--}
+      day21 actualInput `shouldBe` ["989352","430229563871565"])
