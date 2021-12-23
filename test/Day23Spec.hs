906920ce2ce442b61fd5c680e21bb30601c86534
Tom Jemmett <thomas.jemmett@nhs.net>
2021-12-23 23:00:57 +0000

    disables part 2 solution, does not run well

--- test/Day23Spec.hs
+++ test/Day23Spec.hs
@@ -11,9 +11,11 @@ testInput = "#############\n\
 spec :: Spec
 spec = describe "Day 23" $ do
   it "Sample" $ do
-    day23 testInput `shouldBe` ["12521", "44169"]
+    -- day23 testInput `shouldBe` ["12521", "44169"]
+    day23 testInput `shouldBe` ["12521", "12521"]
 
   it "Actual" $ do
     withFile "inputs/day23.txt" ReadMode (\h -> do
       actualInput <- hGetContents h
-      day23 actualInput `shouldBe` ["18195","50265"])
+      -- day23 actualInput `shouldBe` ["18195","50265"])
+      day23 actualInput `shouldBe` ["18195","18195"])
